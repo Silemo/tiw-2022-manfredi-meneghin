f3a62938ad3ef5a7c9b7b8c92602679f14f8a441
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-08-15 15:39:37 +0200

    Added GoToLogin and GoToRegister servlets

---
+++ TIW-2022-manfredi-meneghin-pureHTML/src/main/java/it/polimi/tiw/pureHTML/controllers/GoToLoginPage.java
@@ -0,0 +1,65 @@
+package it.polimi.tiw.pureHTML.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import it.polimi.tiw.pureHTML.utils.PathHelper;
+import it.polimi.tiw.pureHTML.utils.TemplateHandler;
+
+/**
+ * Servlet implementation class GoToLoginPage
+ */
+@WebServlet("/GoToLoginPage")
+public class GoToLoginPage extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GoToLoginPage() {
+    	
+        super();
+    }
+
+    @Override
+    public void init() throws ServletException {
+    	
+    	ServletContext servletContext = getServletContext();
+		this.templateEngine = TemplateHandler.getEngine(servletContext, ".html");
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		forward(request, response, PathHelper.pathToLoginPage);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+	
+	private void forward(HttpServletRequest request, HttpServletResponse response, String path) throws ServletException, IOException{
+		
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		templateEngine.process(path, ctx, response.getWriter());
+		
+	}
+}
