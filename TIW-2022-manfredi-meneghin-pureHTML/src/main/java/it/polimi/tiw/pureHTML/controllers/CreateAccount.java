42c033373c67d769f9b813e04aaacaa82301b539
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-08-16 18:23:07 +0200

    Added CreateAccount servlet

---
+++ TIW-2022-manfredi-meneghin-pureHTML/src/main/java/it/polimi/tiw/pureHTML/controllers/CreateAccount.java
@@ -0,0 +1,119 @@
+package it.polimi.tiw.pureHTML.controllers;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import it.polimi.tiw.pureHTML.beans.User;
+import it.polimi.tiw.pureHTML.dao.AccountDAO;
+import it.polimi.tiw.pureHTML.utils.ConnectionHandler;
+import it.polimi.tiw.pureHTML.utils.PathHelper;
+import it.polimi.tiw.pureHTML.utils.TemplateHandler;
+
+/**
+ * Servlet implementation class CreateAccount
+ */
+@WebServlet("/CreateAccount")
+public class CreateAccount extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	private Connection connection;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateAccount() {
+        
+    	super();
+    }
+    
+    @Override
+    public void init() throws ServletException {
+    	
+    	ServletContext servletContext = getServletContext();
+		this.templateEngine = TemplateHandler.getEngine(servletContext, ".html");
+		this.connection = ConnectionHandler.getConnection(servletContext);
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Creates a new account for the user currently logged in
+		
+		HttpSession session = request.getSession(false);
+		User currentUser = (User)session.getAttribute("currentUser");
+		
+		AccountDAO accountDAO = new AccountDAO(connection);
+		
+		// TODO: remove BigDecimal and balance (account should be initialized at 0)
+		BigDecimal balance = new BigDecimal(10);
+		
+		try {
+			
+			accountDAO.createAccount(currentUser.getId(), balance);
+			
+		}catch (SQLException e) {
+			
+			forwardToErrorPage(request, response, e.getMessage());
+			return;		
+		}
+		
+		response.sendRedirect(getServletContext().getContextPath() + PathHelper.goToHomeServletPath);
+	}
+
+	/**
+	 * Forwards to the ErrorPage
+	 * 
+	 * @param request
+	 * @param response
+	 * @param error
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void forwardToErrorPage(HttpServletRequest request, HttpServletResponse response, String error) throws ServletException, IOException{
+		
+		request.setAttribute("error", error);
+		forward(request, response, PathHelper.pathToErrorPage);
+		return;
+	}
+	
+	/**
+	 * Forwards to the specified path
+	 * 
+	 * @param request
+	 * @param response
+	 * @param path
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void forward(HttpServletRequest request, HttpServletResponse response, String path) throws ServletException, IOException{
+		
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		templateEngine.process(path, ctx, response.getWriter());
+		
+	}
+}
