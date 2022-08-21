b3a172bab5f6664a2b60428a18891290b60b0abc
Giovanni Manfredi <giovanni2.manfredi@mail.polimi.it>
2022-08-21 16:43:20 +0200

    Adapted filters to RIA application and added NoCacher

--- TIW-2022-manfredi-meneghin-RIA/src/main/java/it/polimi/tiw/projects/filters/CheckLoggedUser.java
+++ TIW-2022-manfredi-meneghin-RIA/src/main/java/it/polimi/tiw/projects/filters/CheckLoggedUser.java
@@ -3,37 +3,24 @@ package it.polimi.tiw.projects.filters;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import it.polimi.tiw.projects.utils.PathHelper;
 
 /**
  * Servlet Filter implementation class CheckLoggedUser
  */
-@WebFilter("/CheckLoggedUser")
 public class CheckLoggedUser implements Filter {
 
-	private TemplateEngine templateEngine;
-
 	/**
-	 * @see HttpFilter#HttpFilter()
+	 * Default constructor.
 	 */
 	public CheckLoggedUser() {
-		super();
 		// TODO Auto-generated constructor stub
 	}
 
@@ -47,54 +34,23 @@ public class CheckLoggedUser implements Filter {
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
 		HttpSession s = req.getSession(false);
 
 		if (s != null) {
+			
 			Object user = s.getAttribute("currentUser");
+			
 			if (user != null) {
+				
 				chain.doFilter(request, response);
 				return;
 			}
 		}
 
-		req.setAttribute("error", "You are not authorized to access this page");
-		forward(req, res, PathHelper.pathToErrorPage);
-
-	}
-
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-
-		ServletContext servletContext = fConfig.getServletContext();
-		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
-		templateResolver.setTemplateMode(TemplateMode.HTML);
-		this.templateEngine = new TemplateEngine();
-		this.templateEngine.setTemplateResolver(templateResolver);
-		templateResolver.setSuffix(".html");
-	}
-	
-	/**
-	 * Forwards to the specified path
-	 * 
-	 * @param request
-	 * @param response
-	 * @param path
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	public void forward(HttpServletRequest request, HttpServletResponse response, String path)
-			throws ServletException, IOException {
-
-		ServletContext servletContext = request.getServletContext();
-		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-		templateEngine.process(path, ctx, response.getWriter());
-
+		res.sendRedirect(req.getServletContext().getContextPath() + PathHelper.pathToLoginPage);
 	}
 }
--- TIW-2022-manfredi-meneghin-RIA/src/main/java/it/polimi/tiw/projects/filters/NoCacher.java
+++ TIW-2022-manfredi-meneghin-RIA/src/main/java/it/polimi/tiw/projects/filters/NoCacher.java
@@ -3,26 +3,23 @@ package it.polimi.tiw.projects.filters;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import it.polimi.tiw.projects.utils.PathHelper;
 
 /**
- * Servlet Filter implementation class CheckNotLoggedUser
+ * Servlet Filter implementation class NoCacher
  */
-public class CheckNotLoggedUser implements Filter {
-
+public class NoCacher implements Filter {
+       
 	/**
-	 * Default constructor.
-	 */
-	public CheckNotLoggedUser() {
-		// TODO Auto-generated constructor stub
-	}
+     * Default constructor. 
+     */
+    public NoCacher() {
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see Filter#destroy()
@@ -35,22 +32,23 @@ public class CheckNotLoggedUser implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
-		HttpServletRequest req = (HttpServletRequest) request;
+		
 		HttpServletResponse res = (HttpServletResponse) response;
-		HttpSession s = req.getSession(false);
-
-		if (s != null) {
-			
-			Object user = s.getAttribute("currentUser");
-			
-			if (user != null) {
-				
-				res.sendRedirect(request.getServletContext().getContextPath() + PathHelper.pathToHomePage);
-				return;
-			}
-		}
-
+		res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+		res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+		res.setHeader("Expires", "0"); // Proxies.
+		
+		// pass the request along the filter chain
 		chain.doFilter(request, response);
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
 	}
 }
